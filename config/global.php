9e2d76d0985ded576e7bd712d1e5038122c3bb4d
Florian Steiner <fs@adiwidjaja.com>
2016-06-22 14:06:10 +0200

    Updated commands, added global config

---
+++ config/global.php
@@ -0,0 +1,9 @@
+<?php
+
+//define('FORM_TEMPLATES', __DIR__.'/../views/formtemplates/');
+
+// Authentication
+define('AUTH_HASH', 'sha256');
+define('AUTH_TABLE', 'users');
+define('AUTH_USERNAME_COLUMN', 'username');
+define('AUTH_PASSWORD_COLUMN', 'password');
